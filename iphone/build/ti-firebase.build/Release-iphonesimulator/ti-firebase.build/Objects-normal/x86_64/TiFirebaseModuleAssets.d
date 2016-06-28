5f349f54b69604b84e5892e54c3ed00739440ef2
Hans Knoechel <hans.knoechel@hs-osnabrueck.de>
2016-06-28 10:48:38 -0700

    Fix framework search-paths, remove build from git

--- iphone/build/ti-firebase.build/Release-iphonesimulator/ti-firebase.build/Objects-normal/x86_64/TiFirebaseModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Classes/TiFirebaseModuleAssets.m \
-  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Classes/TiFirebaseModuleAssets.h
