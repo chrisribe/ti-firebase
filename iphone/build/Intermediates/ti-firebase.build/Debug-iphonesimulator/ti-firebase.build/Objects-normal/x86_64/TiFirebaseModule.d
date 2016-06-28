8f4b4635b7f1355f8693380235d2e962310c43ca
ChrisRibe <chrisribe@users.noreply.github.com>
2016-06-28 15:37:46 -0400

    Revert "Fix framework search-paths, remove build from git"

---
+++ iphone/build/Intermediates/ti-firebase.build/Debug-iphonesimulator/ti-firebase.build/Objects-normal/x86_64/TiFirebaseModule.d
@@ -0,0 +1,47 @@
+dependencies: \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Classes/TiFirebaseModule.m \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Classes/TiFirebaseModule.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiModule.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiProxy.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiEvaluator.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/KrollContext.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiToJS.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiCore.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/Ti.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiBase.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiContextRef.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiObjectRef.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiValueRef.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/WebKitAvailability.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiStringRef.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/JavaScriptCore/TiStringRefCF.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/KrollCallback.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/KrollObject.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiBindingRunLoop.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiBase.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/defines.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiThreading.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiPublicAPI.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiUtils.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiDimension.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiColor.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/WebFont.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiFile.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiBuffer.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiBlob.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiHost.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/Bridge.h \
+  /Users/cribe/Library/Application\ Support/Titanium/mobilesdk/osx/5.3.0.GA/iphone/include/TiStylesheet.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/Firebase.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FirebaseAnalytics.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRAnalytics+AppDelegate.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRAnalytics.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIREventNames.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRParameterNames.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRUserPropertyNames.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRAnalyticsConfiguration.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRApp.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIRConfiguration.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseAnalytics.framework/Headers/FIROptions.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseInstanceID.framework/Headers/FirebaseInstanceID.h \
+  /Users/cribe/Documents/github-projects/ti-firebase/iphone/Resources/FirebaseInstanceID.framework/Headers/FIRInstanceID.h
