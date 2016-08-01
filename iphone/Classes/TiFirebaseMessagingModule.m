2253a146d7d86f21ef9f0b5da2ea02e55b3bc66c
chrisribe <christian.ribe@gmail.com>
2016-08-01 14:25:09 -0400

    Added empty Messaging module + required libraries
     - Empty module to allow development by others….

---
+++ iphone/Classes/TiFirebaseMessagingModule.m
@@ -0,0 +1,36 @@
+/**
+ * ti-firebase Messaging module
+ *
+ * Created by Your Name
+ * Copyright (c) 2016 Your Company. All rights reserved.
+ */
+
+#import "TiFirebaseModule.h"
+#import "TiFirebaseMessagingModule.h"
+#import "TiBase.h"
+#import "TiHost.h"
+#import "TiUtils.h"
+
+@implementation TiFirebaseMessagingModule
+
+-(void)dealloc
+{
+	[super dealloc];
+}
+
+#pragma mark Internal Memory Management
+
+-(void)didReceiveMemoryWarning:(NSNotification*)notification
+{
+	[super didReceiveMemoryWarning:notification];
+}
+
+
+#pragma Public APIs
+
+//For api implement notes:
+//Good luck :)
+//https://firebase.google.com/docs/cloud-messaging/ios/client
+
+
+@end
---
+++ iphone/TiFirebaseMessagingModule.h
@@ -0,0 +1,15 @@
+/**
+ * ti-firebase Messaging module
+ *
+ * Created by Your Name
+ * Copyright (c) 2016 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+#import "Firebase.h"
+
+@interface TiFirebaseMessagingModule : TiModule
+{
+}
+
+@end
